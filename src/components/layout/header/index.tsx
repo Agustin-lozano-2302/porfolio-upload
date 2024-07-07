e58ea8e6af2cc022ff96520a323f40a00ea3986d
Agustin Lozano <agustin.lozano@itrocksweb.com>
2024-07-07 18:29:12 -0300

    fix-images-and-padding

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -22,11 +22,17 @@ export default function Header() {
       {actualWidth < 700 ?
         <div className="relative">
           {openMenu ?
-            <img className="h-10 m-5" src="/images/close.svg" alt="menu" onClick={() => setOpenMenu(!openMenu)} />
+            <svg className="h-8 m-5" onClick={() => setOpenMenu(!openMenu)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 5L19 19M5 19L19 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
             :
-            <img className="h-10 m-5" src="/images/burger-menu.svg" alt="menu" onClick={() => setOpenMenu(!openMenu)} />
+            <svg onClick={() => setOpenMenu(!openMenu)} className="h-8 m-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+              <path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+              <path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+            </svg>
           }
-          {openMenu && <div className="flex flex-col w-44 absolute left-[-50px] max-h-72 gap-4 justify-center animate-fade-down animate-duration-1000 bg-white p-3">
+          {openMenu && <div className="flex flex-col w-44 absolute left-[-50px] max-h-72 gap-4 justify-center animate-fade-down animate-duration-1000 bg-white p-3 rounded-lg">
             <Headling link="#about-me" fontW="font-light" title={"Sobre mí"} />
             <Headling link="#skills" fontW="font-light" title={"Experiencia"} />
             {/* <Headling link="#projects" fontW="font-light" title={"Proyectos"} /> */}
